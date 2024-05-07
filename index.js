fb2bf5cdc3dd1564562f1a0e86a7e4e3af061efb
sridhar02 <kattasridhar02@gmail.com>
2024-05-07 21:04:47 +0530

    node puppeteer setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import { scrapeLogic } from "./scrapeLogic.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
